124e3c4e5a7b55304eb03c0122b5537f58aa0ff0
Sean Innes <sean.r.innes@googlemail.com>
2018-11-19 17:00:14 +0000

    Add initial tetriminos

--- ghg/src/Lib.hs
+++ ghg/src/Lib.hs
@@ -13,15 +13,60 @@ data Game = Play {for   :: Foreground,
                   fall  :: FallingBlock,
                   word  :: String}
           | Menu {menu :: Menu}
+          deriving Show
 
-data Tetramino = I | O | T | S | Z | J | L
+-- initial_game width height = Play 
+--   where
+--     for  = Foreground ((map . map) (const Nothing) [[1..width] | _ <- [1..height]])
+--     back = Background ((map . map) (const Nothing) [[1..width] | _ <- [1..height]])
+--     wtf  = "hello"
+--     fall = FallingBlock I [(10, 10), (10, 11), (10, 12), (10, 13)]
+--     word = ""
 
-data Foreground = Foreground [[Maybe Tetramino]]
+data Tetramino = I | O | T | S | Z | J | L deriving Show
 
-data Background = Background [[Maybe (Char, Int)]]
+data Foreground = Foreground [[Maybe Tetramino]] deriving Show
 
-data Word2Find = Word2Find String
+data Background = Background [[Maybe (Char, Int)]] deriving Show
 
-data FallingBlock = FallingBlock Tetramino Int [(Int,Int)]
+data Word2Find = Word2Find String deriving Show
 
-data Menu = M Int
+data FallingBlock = FallingBlock Tetramino (Float, Float) Rotation deriving Show
+
+data Rotation = North | South | East | West deriving Show
+
+
+-- Rotation about the origin
+rotRight90 :: (Float, Float) -> (Float, Float)
+rotRight90 (y, x) = (x, -y)
+
+rot180 = rotRight90 . rotRight90
+
+rotLeft90 = rotRight90 . rotRight90 . rotRight90
+
+-- Rotate p about p'
+rotRight90Point :: (Float, Float) -> (Float, Float) -> (Float, Float)
+rotRight90Point (y', x') (y, x) = (\(y, x) -> (y + y', x + x')) $ rotRight90 (y - y', x - x')
+
+rot180Point p' = rotRight90Point p' . rotRight90Point p'
+rotLeft90Point p' = rotRight90Point p' . rotRight90Point p' . rotRight90Point p'
+
+
+
+basePoints :: Tetramino -> [(Float, Float)]
+basePoints t = 
+  case t of
+    I -> [(-1.5, 0.5), (-0.5, 0.5), (0.5, 0.5), (1.5, 0.5)]
+    J -> [(-1, 0), (0, 0), (1, 0), (1 , -1)]
+    L -> [(-1, 0), (0, 0), (1, 0), (1 , 1)]
+    O -> [(0.5, 0.5), (-0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
+    S -> [(-1, 0), (0, 0), (-1, 1), (0 , -1)]
+    T -> [(-1, 0), (0, 0), (0, 1), (0 , -1)]
+    Z -> [(-1, 0), (0, 0), (-1, -1), (0 , 1)]
+
+-- blockPoints :: FallingBlock -> [(Int, Int)]
+-- blockPoints (FallingBlock tetramino center rotation) =
+  -- case tetramino of
+    -- I -> 
+
+data Menu = M Int deriving Show
