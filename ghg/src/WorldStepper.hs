fd3b0ae45d08035c8084283230a5931cfaf095e8
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2018-11-19 16:40:47 +0000

    Refactored worldStepper

--- ghg/src/WorldStepper.hs
+++ ghg/src/WorldStepper.hs
@@ -1,6 +1,7 @@
 module WorldStepper (worldStepper) where
 
 import Lib
+import Data.Maybe
 
 speed :: Int
 speed = 2
@@ -8,12 +9,16 @@ speed = 2
 worldHeight :: Int
 worldHeight = 18
 
-hasCollided :: [[Maybe Tetramino]] -> [(Int, Int)] -> Bool
-hasCollided tetras = foldr f k
+worldWidth :: Int
+worldWidth = 10
+
+hasCollided :: Foreground -> [(Int, Int)] -> Bool
+hasCollided (Foreground tetras) = foldr f k
   where
     k = False
     f _ True = True
     f (col, row) False
+      | col < 0 || col >= worldHeight = False
       | row >= worldHeight = False
       | otherwise = isJust ((tetras !! col) !! row)
 
@@ -22,8 +27,16 @@ worldStepper dt (Menu menu) = Menu menu
 worldStepper dt game = game { for = for'
                             , back = back'
                             , fall = fall'
-                            , accTime = acc }
+                            , accTime = accTime' }
   where
-    fall' = (fall game) { tetShape = tetShape' }
-    tetShape' = [(col+1, row) | (col, row) <- tetShape (fall game)]
+    (Play for' back' wtf' fall' accTime') = game
+    
+    fall'' = fall' { tetShape = chosenTetShape }
+
+    chosenTetShape = if hasCollided for' tetShape''
+                        then tetShape fall'
+                        else tetShape''
+                             
+    tetShape'' = [(col+1, row) | (col, row) <- tetShape fall']
     acc = accTime game + dt
+
