a2a8f0bfe2de623ebce50b695877947b1a6540a1
Jamie Luxford <jl15219@my.bristol.ac.uk>
2018-11-19 16:25:48 +0000

    Background and Foreground Done

--- ghg/src/Renderer.hs
+++ ghg/src/Renderer.hs
@@ -1,7 +1,46 @@
 module Renderer (game2Pic) where
 
 import Lib
-    
+
+blockSize :: Float
+blockSize = 10
+
+
 game2Pic :: Game -> Picture
-game2Pic (Menu x) = Blank
-game2Pic (Play{}) = Blank
+game2Pic (Menu x) = rendMenu x
+game2Pic (Play {for = f,back = b, fall = p}) = Pictures (rendBack b ++ rendFor f ++ rendFall p)
+
+rendMenu :: Menu -> Picture
+rendMenu (M i) = Blank
+
+rendBack :: Background -> [Picture]
+rendBack (Background ps) = arrayBasic (fmap (fmap f) ps) where
+  f :: (Maybe Char, Int) -> Picture
+  f (_, x) = Color (makeColor 1 1 1 (g x)) (Polygon [(0,0),(0,blockSize),(blockSize,blockSize),(blockSize,0)])
+  g :: Int -> Float
+  g x = exp (-(((fromIntegral x) + 1)/ 3))
+
+rendFor :: Foreground -> [Picture]
+rendFor (Foreground ps) = arrayBasic (fmap (fmap f) ps) where
+  f :: Maybe Tetramino -> Picture
+  f (Just x) = rendTetramino x (f Nothing)
+  f Nothing  = (Polygon [(0,0),(0,blockSize),(blockSize,blockSize),(blockSize,0)])
+
+rendFall :: FallingBlock -> [Picture]
+rendFall (FallingBlock t i is) = [Blank]
+
+--makeColor red green blue alpha
+
+rendTetramino :: Tetramino -> Picture -> Picture
+rendTetramino I p = Color (makeColor 0 1 1 1) p
+rendTetramino O p = Color (makeColor 1 1 0 1) p
+rendTetramino T p = Color (makeColor 1 0 1 1) p
+rendTetramino S p = Color (makeColor 0 1 0 1) p
+rendTetramino Z p = Color (makeColor 1 0 0 1) p
+rendTetramino J p = Color (makeColor 0 0 1 1) p
+rendTetramino L p = Color (makeColor 1 0.5 0 1) p
+
+arrayBasic :: [[Picture]] -> [Picture]
+arrayBasic = foldr f [] where
+  f :: [Picture] -> [Picture] -> [Picture]
+  f xs ps = (fmap (Translate 0 blockSize) ps) ++ (map (\(a,b) -> (Translate a 0 b)) (zip [0..] xs))
