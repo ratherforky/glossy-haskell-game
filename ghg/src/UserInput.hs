9e520d18785d9bd3a6a0f3e698addccdf0eee2e3
Sean Innes <sean.r.innes@googlemail.com>
2018-11-19 15:18:41 +0000

    Convert input to a move

--- ghg/src/UserInput.hs
+++ ghg/src/UserInput.hs
@@ -1,19 +1,32 @@
 module UserInput (eventHandler) where
 
-import Lib
+import Prelude hiding (Right, Left)
+import Lib hiding (Rotate)
+import Debug.Trace
 
-data Direction = Left | Right
+data Direction = Left | Right deriving Show
 
 data Move = Rotate Direction
           | Move Direction
           | FastDrop
           | WordInput Char
+          deriving Show
 
 eventHandler :: Event -> Game -> Game
 eventHandler e = update (getMove e)
 
-getMove :: Event -> (Maybe Move)
-getMove = undefined
+getMove :: Event -> Maybe Move
+getMove (EventKey (SpecialKey key) Down _ _) = 
+  case key of
+    KeyUp    -> Just (Rotate Right)
+    KeyDown  -> Just (Rotate Left)
+    KeyRight -> Just (Move Right)
+    KeyLeft  -> Just (Move Left)
+    KeySpace -> Just FastDrop
+    _        -> Nothing
+getMove (EventKey (Char c) Down _ _) = Just (WordInput c)
+getMove _ = Nothing
 
 update :: Maybe Move -> Game -> Game
-update = undefined
+update (Just x) g = traceShow (Just x) g
+update Nothing g = g
