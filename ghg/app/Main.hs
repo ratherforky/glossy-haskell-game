67a8f02dff21c25eb3aa4b6d82a91ff7252a45f4
Jamie Luxford <jl15219@my.bristol.ac.uk>
2018-11-19 12:57:10 +0000

    Basic datatypes and example test code

--- ghg/app/Main.hs
+++ ghg/app/Main.hs
@@ -2,5 +2,29 @@ module Main where
 
 import Lib
 
+data Game = Play {for   :: Foreground,
+                  back  :: Background,
+                  wtf   :: Word2Find,
+                  fall  :: FallingBlock}
+          | Menu {menu :: Menu}
+
+data Tetramino = L | I | T | S | Z | B
+
+data Foreground = Foreground [[Maybe Tetramino]]
+
+data Background = Background [[Maybe (Char, Int)]]
+
+data Word2Find = Word2Find String
+
+data FallingBlock = FallingBlock Tetramino Int [(Int,Int)]
+
+data Menu = M Int
+
 main :: IO ()
-main = someFunc
+main = play FullScreen black 60 (Menu (M 0)) game2Pic eventHandler worldStepper
+
+game2Pic :: Game -> Picture
+
+eventHandler :: Event -> Game -> Game
+
+worldStepper :: Float -> Game -> Game
